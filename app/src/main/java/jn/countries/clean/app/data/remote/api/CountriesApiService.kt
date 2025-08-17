b1736e742338b95c02611d836144caad11601398
Jaison Villarroel <xyeison0x@gmail.com>
2025-08-17 21:12:24 +0200

    feat: implement use case to get countries

--- app/src/main/java/jn/countries/clean/app/data/remote/api/CountriesApiService.kt
+++ app/src/main/java/jn/countries/clean/app/data/remote/api/CountriesApiService.kt
@@ -1,7 +1,6 @@
 package jn.countries.clean.app.data.remote.api
 
 import jn.countries.clean.app.data.remote.dto.CountryDto
-import kotlinx.coroutines.flow.Flow
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
